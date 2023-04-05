5756683b34c6b603419eac9576e28b5126fe39a0
libacchu <libacchu@students.42wolfsburg.de>
2023-04-06 01:18:07 +0200

    added protection for '\0'

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 15:32:52 by libacchu          #+#    #+#             */
-/*   Updated: 2022/04/06 13:02:31 by libacchu         ###   ########.fr       */
+/*   Updated: 2023/04/06 00:28:54 by libacchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n)
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
